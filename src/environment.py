e4cce6105d56249adc9f1623e49d52782bfdc171
Zhihan Gao <zhihan.gao@yahoo.com>
2021-01-06 23:01:23 -0500

    update running environment

--- src/environment.py
+++ src/environment.py
@@ -1,9 +1,20 @@
 """ Module for running environment"""
 
-class Env:
+from .bandit import Bandit
+
+class IndEnv:
+    """Independent Running Environment for Strategies.
+    """
+
     def __init__(self, rounds, bandit):
+        if rounds <= 0 or rounds != int(rounds):
+            raise ValueError('rounds must be a positive integer.')
         self.rounds = rounds
+
+        if not isinstance(bandit, Bandit):
+            raise ValueError('bandit must be an object of class Bandit.')
         self.bandit = bandit
+        self.num_machines = self.bandit.num_machines()
         self.strategy = {}
         self.result = {}
 
@@ -14,25 +25,43 @@ class Env:
             raise ValueError('{} already exists.'.format(strategy_name))
 
 
+    def clear_strategy(self):
+        self.strategy = {}
+        self.result = {}
+
+
     def run(self):
+        if len(self.strategy) == 0:
+            return
+
         config = {}
         config['rounds'] = self.rounds
-        config['num_machines'] = self.bandit.num_machines()
+        config['num_machines'] = self.num_machines
 
-        for name, strategy in self.strategy.items():
-            rewards = []
-            selections = []
+        history = {}
 
-            for i in range(self.rounds):
-                history = {}
-                history['rewards'] = [ _ for _ in rewards]
-                history['selections'] = [ _ for _ in selections]
-                curr_selection = strategy(history, config)
-                curr_reward = self.bandit.pull(curr_selection)
+        for i in range(self.rounds):
+            if i == 0:
+                for name in self.strategy:
+                    history[name] = {}
+                    history[name]['reward'] = []
+                    history[name]['selection'] = []
 
-                rewards.append(curr_reward)
-                selections.append(curr_selection)
+            curr_selection_dict = {}
 
-            self.result[name] = {}
-            self.result[name]['rewards'] = rewards
-            self.result[name]['selections'] = selections
+            for name, strategy in self.strategy.items():
+                curr_selection_dict[name] = strategy(history[name], config)
+
+            # ensure same pull would return same reward
+            selection2reward_dict = {}
+            for name in curr_selection_dict:
+                curr_selection = curr_selection_dict[name]
+
+                if curr_selection not in selection2reward_dict:
+                    selection2reward_dict[curr_selection] = self.bandit.pull(curr_selection)
+
+            for name in self.strategy:
+                history[name]['reward'].append(selection2reward_dict[curr_selection_dict[name]])
+                history[name]['selection'].append(curr_selection_dict[name])
+
+        self.result = history
