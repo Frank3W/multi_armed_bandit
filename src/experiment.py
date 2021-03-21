5e53cf30d06b25b69768c563e3e88d55a204fd01
Zhihan Gao <zhihan.gao@yahoo.com>
2021-03-21 09:11:50 -0400

    add paralleling computation for experiments

--- src/experiment.py
+++ src/experiment.py
@@ -1,21 +1,16 @@
+import joblib
+from joblib import Parallel, delayed
 
 class Experiment:
     def __init__(self, run_env):
         self.run_env = run_env
 
+    def one_run(self):
+        self.run_env.run()
+        return self.run_env.result
 
-    def get_reward_selection(self, num_runs):
-        result_dict = {}
 
-        for i in range(num_runs):
-            self.run_env.run()
-            for strategy_name in self.run_env.result:
-                if i == 0:
-                    result_dict[strategy_name] = {}
-                    result_dict[strategy_name]['reward'] = []
-                    result_dict[strategy_name]['selection'] = []
+    def multiple_run(self, num_runs, n_jobs=1):
 
-                result_dict[strategy_name]['reward'].append(self.run_env.result[strategy_name]['reward'])
-                result_dict[strategy_name]['selection'].append(self.run_env.result[strategy_name]['selection'])
-
-        return result_dict
+        result = Parallel(n_jobs=n_jobs)(delayed(self.one_run)() for i in range(num_runs))
+        return result
